31dac831ce2ae0b8eb0d89cc8c98582c244fd8e5
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-12 02:20:32 +0100

    Initialize snyk-upgrade-6147fd689e35a7bd8ada125ae13dc9aa

---
+++ packages/next/src/server/lib/recursive-readdir-sync.ts
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import { join } from 'path'
+
+/**
+ * Recursively read directory
+ * Returns array holding all relative paths
+ */
+export function recursiveReadDirSync(
+  /** The directory to read */
+  dir: string,
+  /** This doesn't have to be provided, it's used for the recursion */
+  arr: string[] = [],
+  /** Used to replace the initial path, only the relative path is left, it's faster than path.relative. */
+  rootDir = dir
+): string[] {
+  const result = fs.readdirSync(dir, { withFileTypes: true })
+
+  result.forEach((part) => {
+    const absolutePath = join(dir, part.name)
+
+    if (part.isDirectory()) {
+      recursiveReadDirSync(absolutePath, arr, rootDir)
+      return
+    }
+    arr.push(absolutePath.replace(rootDir, ''))
+  })
+
+  return arr
+}
