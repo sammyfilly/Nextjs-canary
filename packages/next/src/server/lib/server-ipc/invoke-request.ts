118ac6563ffe2ee710591e8a83ea775cb62d1822
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-12 02:27:34 +0100

    Initialize patch-2

---
+++ packages/next/src/server/lib/server-ipc/invoke-request.ts
@@ -0,0 +1,71 @@
+import '../../node-polyfill-fetch'
+
+import type { IncomingMessage } from 'http'
+import type { Writable, Readable } from 'stream'
+import { filterReqHeaders } from './utils'
+
+export const invokeRequest = async (
+  targetUrl: string,
+  requestInit: {
+    headers: IncomingMessage['headers']
+    method: IncomingMessage['method']
+  },
+  readableBody?: Readable | ReadableStream
+) => {
+  // force to 127.0.0.1 as IPC always runs on this hostname
+  // to avoid localhost issues
+  const parsedTargetUrl = new URL(targetUrl)
+  parsedTargetUrl.hostname = '127.0.0.1'
+
+  const invokeHeaders = filterReqHeaders({
+    'cache-control': '',
+    ...requestInit.headers,
+  }) as IncomingMessage['headers']
+
+  const invokeRes = await fetch(parsedTargetUrl.toString(), {
+    headers: invokeHeaders as any as Headers,
+    method: requestInit.method,
+    redirect: 'manual',
+
+    ...(requestInit.method !== 'GET' &&
+    requestInit.method !== 'HEAD' &&
+    readableBody
+      ? {
+          body: readableBody as BodyInit,
+          duplex: 'half',
+        }
+      : {}),
+
+    next: {
+      // @ts-ignore
+      internal: true,
+    },
+  })
+
+  return invokeRes
+}
+
+export async function pipeReadable(
+  readable: ReadableStream,
+  writable: Writable
+) {
+  const reader = readable.getReader()
+
+  async function doRead() {
+    const item = await reader.read()
+
+    if (item?.value) {
+      writable.write(Buffer.from(item?.value))
+
+      if ('flush' in writable) {
+        ;(writable as any).flush()
+      }
+    }
+
+    if (!item?.done) {
+      return doRead()
+    }
+  }
+  await doRead()
+  writable.end()
+}
